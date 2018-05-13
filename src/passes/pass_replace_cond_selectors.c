210102beb2904f201f924042259937d1b73b2dc9
tnstrssnr <stroessner.tinamaria@gmail.com>
2018-05-13 14:36:23 +0200

    replace cond nodes one by one

--- src/passes/pass_replace_cond_selectors.c
+++ src/passes/pass_replace_cond_selectors.c
@@ -1,28 +1,26 @@
 #include <libfirm/firm.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
+#include <node_container.h>
 
+void pass_replace_cond_selectors(ir_graph* irg, void* data) {
+    
+    ir_node_container* container = malloc(sizeof(ir_node_container));
+    container->func = is_Cond;
+    collect_nodes(irg, container);
 
-/**
- * Pass substitutes all Cond selectors w/ Const nodes
- */
-void substitute_bools_walk(ir_node* node, void* data) {
+    if (container->nodes_n > 0) {
 
-    if(is_Cond(node)) {
-        int rand_num = rand() % 2;
-        ir_tarval* val = rand_num ? get_tarval_b_true() : get_tarval_b_false();
+        srand(time(NULL));
+        ir_node* node = container->nodes[rand() % container->nodes_n];
+        ir_tarval* val = rand() % 2 ? get_tarval_b_true() : get_tarval_b_false();
         dbg_info * dbgi = get_irn_dbg_info(node);
-        ir_graph* irg = get_irn_irg(node);
         ir_node* const_node = new_rd_Const(dbgi, irg, val);
-        
+    
         set_Cond_selector(node, const_node);
     }
 
-}
-
-void pass_replace_cond_selectors(ir_graph* irg, void* data) {
-    srand(time(NULL));
-    irg_walk_graph(irg, substitute_bools_walk, NULL, NULL);
-    
+    free(container);
 
 }
